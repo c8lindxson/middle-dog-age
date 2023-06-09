657a096f0b217f8d5ad3c13edf0c9695105c83cf
Cai Dixon <city.art.girl@gmail.com>
2023-06-09 19:30:22 +0900

    changed if statement to include less than 10 userAge and added special conditional for userAge 1 and 2

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,25 +6,30 @@ button.addEventListener('click', (event) => {
   console.log('button clicked');
   console.log(event);
   console.log(event.target);
-  const earlyYears = 21;
-  laterYears = parseInt(userAge.value, 10) - 2;
-  laterYears *= 4;
-  let dogYears = earlyYears + laterYears;
-  console.log(dogYears);
-  if (dogYears < 50) {
-      image.src = './images/babypuppy.jpg';
-  } else if (dogYears < 100) {
-      image.src = './images/puppy.jpg'
-  } else if (dogYears < 150) {
-      image.src = './images/young dog.jpg'
-  } else if (dogYears < 200) {
-      image.src = './images/dog.webp'
-  } else if (dogYears < 250) {
-      image.src = './images/middle aged dog.jpeg'
-  } else if (dogYears < 300) {
-      image.src = './images/old dog.jpg'
+  const earlyYears = 10.5;
+    if (parseInt(userAge.value) <= 2) {
+    dogYears = parseInt(userAge.value) * earlyYears;
+
   } else {
-      image.src = './images/super old dog.webp'
+    dogYears = (earlyYears * 2) + (parseInt((userAge.value) - 2) * 4);
+  }
+  console.log(dogYears);
+  if (dogYears >= 350) {
+    image.src = './images/super old dog.webp';
+  }else if (dogYears >= 300) {
+    image.src = './images/old dog.jpg';
+  }else if (dogYears >= 250) {
+    image.src = './images/middle aged dog.jpeg';
+  }else if (dogYears >= 200) {
+    image.src = './images/dog.webp';
+  } else if (dogYears >= 150) {
+    image.src = './images/young dog.jpg';
+  } else if (dogYears >= 100) {
+    image.src = './images/puppy.jpg';
+  } else if (dogYears >= 50) {
+    image.src = './images/babbypuppy.jpg';
+  } else {
+    image.src = './images/preemiepuppy.jpg';
   }
   age.innerHTML = `<h1>You are ${dogYears} years old in dog years!</h1>`;
 });
