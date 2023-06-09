aeb6b957fa422b612ef51431211c77b01ae986d8
Cai Dixon <city.art.girl@gmail.com>
2023-06-09 18:00:58 +0900

    images change now and message is centered

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,43 +1,30 @@
-// This is where it all goes :)
 let userAge = document.getElementById('userAge');
-const button = document.getElementsByClassName('button');
-const image = document.getElementsByClassName('image');
-
-document.addEventListener('click', function handleClick(event) {
+const button = document.querySelector('.button');
+button.addEventListener('click', (event) => {
+  const image = document.querySelector('.image');
+  const age = document.querySelector('.age');
   console.log('button clicked');
   console.log(event);
   console.log(event.target);
-  let userAge = userAge.to_i;
   const earlyYears = 21;
-  laterYears = userAge - 2;
+  laterYears = parseInt(userAge.value, 10) - 2;
   laterYears *= 4;
   let dogYears = earlyYears + laterYears;
-  switch (dogYears) {
-    case (dogYears < 50) :
-      image = './images/babypuppy.jpg';
-      break;
-    case (dogYears < 100) :
-      image = './images/puppy.jpg'
-      break;
-    case (dogYears < 200) :
-      image = './images/young dog.jpg'
-      break;
-    case (dogYears < 300) :
-      image = './images/dog.webp'
-      break;
-    case (dogYears < 400) :
-      image = './images/middle aged dog.jpeg'
-      break;
-    case (dogYears < 500) :
-      image = './images/old dog.jpg'
-      break;
-    case (dogYears < 600) :
-      image = './images/super old dog.webp'
-      break;
-    default :
-    image = './images/dog-questino.png'
-    break;
+  console.log(dogYears);
+  if (dogYears < 50) {
+      image.src = './images/babypuppy.jpg';
+  } else if (dogYears < 100) {
+      image.src = './images/puppy.jpg'
+  } else if (dogYears < 150) {
+      image.src = './images/young dog.jpg'
+  } else if (dogYears < 200) {
+      image.src = './images/dog.webp'
+  } else if (dogYears < 250) {
+      image.src = './images/middle aged dog.jpeg'
+  } else if (dogYears < 300) {
+      image.src = './images/old dog.jpg'
+  } else {
+      image.src = './images/super old dog.webp'
   }
-  age.insertAdjacentHTML("beforeend", `<h1>You are ${dog_years} years old in dog years!</h1>`);
-  image.insertAdjacentHTML("beforeend", `src="${image}" alt="" `);
+  age.innerHTML = `<h1>You are ${dogYears} years old in dog years!</h1>`;
 });
